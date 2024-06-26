78c7ecf983ec547d6f5accb449fec75b57f46b0f
Ashkar_Noorul <noorulashkar007@gmail.com>
2024-06-26 16:53:53 +0530

    completed

--- DistributedCaching/Program.cs
+++ DistributedCaching/Program.cs
@@ -1,34 +1,32 @@
-
-
 using DistributedCaching.Models;
 using DistributedCaching.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("dotnetSeries"));
 });
+builder.Services.AddTransient<IProductService, ProductService>();
 
-builder.Services.AddMemoryCache();
-builder.Services.AddTransient<IProductService,ProductService>();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost";
+    options.ConfigurationOptions = new StackExchange.Redis.ConfigurationOptions()
+    {
+        AbortOnConnectFail = true,
+        EndPoints = { options.Configuration }
+    };
+});
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options => options.DisplayRequestDuration());
 }
-
-app.UseHttpsRedirection();
-
 
 app.MapGet("/products", async (IProductService service) =>
 {
@@ -48,30 +46,5 @@ app.MapPost("/products", async (ProductCreationDto product, IProductService serv
     return Results.Created();
 });
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
+app.UseHttpsRedirection();
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
--- DistributedCaching/Services/ProductService.cs
+++ DistributedCaching/Services/ProductService.cs
@@ -1,10 +1,11 @@
 ﻿using DistributedCaching.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace DistributedCaching.Services
 {
-    public class ProductService(AppDbContext context, IMemoryCache cache, ILogger<ProductService> logger) : IProductService
+    public class ProductService(AppDbContext context, IDistributedCache cache, ILogger<ProductService> logger) : IProductService
     {
         public async Task Add(ProductCreationDto request)
         {
@@ -23,45 +24,31 @@ namespace DistributedCaching.Services
         {
             var cacheKey = $"product:{id}";
             logger.LogInformation("fetching data for key: {CacheKey} from cache.", cacheKey);
-            if(!cache.TryGetValue(cacheKey, out Product? product)) {
-                logger.LogInformation("cache miss. fetching data for key: {CacheKey} from database.", cacheKey);
-                product = await context.Products.FindAsync(id);
-
-                var cacheOptions = new MemoryCacheEntryOptions()
-                .SetSlidingExpiration(TimeSpan.FromSeconds(30))
-                .SetAbsoluteExpiration(TimeSpan.FromSeconds(300))
-                .SetPriority(CacheItemPriority.Normal);
-                logger.LogInformation("setting data for key: {CacheKey} to cache.", cacheKey);
-                cache.Set(cacheKey, product,cacheOptions);
-            }
-            else
-            {
-                logger.LogInformation("cache hit for key: {CacheKey}.", cacheKey);
-            }
-            return product;
+            var product = await cache.GetOrSetAsync(cacheKey,
+                async () =>
+                {
+                    logger.LogInformation("cache miss. fetching data for key: {CacheKey} from database.", cacheKey);
+                    return await context.Products.FindAsync(id)!;
+                })!;
+            return product!;
         }
 
         public async Task<List<Product>> GetAll()
         {
             var cacheKey = "products";
             logger.LogInformation("fetching data for key: {CacheKey} from cache.", cacheKey);
-            if (!cache.TryGetValue(cacheKey, out List<Product>? products))
-            {
-                logger.LogInformation("cache miss. fetching data for key: {CacheKey} from database.", cacheKey);
-                products = await context.Products.ToListAsync();
-                var cacheOptions = new MemoryCacheEntryOptions()
-                    .SetSlidingExpiration(TimeSpan.FromSeconds(30))
-                    .SetAbsoluteExpiration(TimeSpan.FromSeconds(300))
-                    .SetPriority(CacheItemPriority.NeverRemove)
-                    .SetSize(2048);
-                logger.LogInformation("setting data for key: {CacheKey} to cache.", cacheKey);
-                cache.Set(cacheKey, products, cacheOptions);
-            }
-            else
-            {
-                logger.LogInformation("cache hit for key: {CacheKey}.", cacheKey);
-            }
-            return products;
+            
+            var cacheOptions = new DistributedCacheEntryOptions()
+                .SetAbsoluteExpiration(TimeSpan.FromMinutes(20))
+                .SetSlidingExpiration(TimeSpan.FromMinutes(2));
+
+            var products = await cache.GetOrSetAsync(cacheKey,
+                async () =>
+                {
+                    logger.LogInformation("cache miss. fetching data for key: {CacheKey} from database.", cacheKey);
+                    return await context.Products.ToListAsync();
+                },cacheOptions)!;
+            return products!;
         }
     }
 }
